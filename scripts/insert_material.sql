79e8df9cf92cbb9d98af51b7db6825dd82c875aa
mujiks <mujiks@netcabo.pt>
2019-11-25 15:01:20 +0000

    Fix ids

--- scripts/insert_material.sql
+++ scripts/insert_material.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.material(id, material_type, quantity) VALUES (1, 'BOOK', 5);
-INSERT INTO public.material(id, material_type, quantity) VALUES (1, 'MONITOR', 3);
-INSERT INTO public.material(id, material_type, quantity) VALUES (1, 'LAPTOP', 2);
+INSERT INTO public.material(id, material_type, quantity) VALUES (2, 'MONITOR', 3);
+INSERT INTO public.material(id, material_type, quantity) VALUES (3, 'LAPTOP', 2);
 
